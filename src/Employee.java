deb57b4d44e88d6ef8f30fd203a11bcf99055fbe
Parker782001 <114192070+Parker782001@users.noreply.github.com>
2024-04-22 09:12:21 -0400

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,17 +1,44 @@
 public class Employee extends User {
 
-    private String strRole;
+    private int employeeID;
+    private String position;
+    private double salary;
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
     @Override
     public String toString() {
-        return "Employee{" + "strRole=" + strRole + '}';
-    }
-
-    public String getStrRole() {
-        return strRole;
-    }
-
-    public void setStrRole(String strRole) {
-        this.strRole = strRole;
+        return "Employee{" +
+                "employeeID=" + employeeID +
+                ", position='" + position + '\'' +
+                ", salary=" + salary +
+                ", intID=" + getIntID() +
+                ", strFirstName='" + getStrFirstName() + '\'' +
+                ", strLastName='" + getStrLastName() + '\'' +
+                ", strUsername='" + getStrUsername() + '\'' +
+                ", strPassword='" + getStrPassword() + '\'' +
+                '}';
     }
 }
